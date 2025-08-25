31ee14bb26bf6ee807013978510b42ecfa61b09a
PKPM 构力结构 <13466632+pkpmgh@user.noreply.gitee.com>
2025-08-25 09:34:16 +0800

    改配置试试

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix: "./",
+  images: { unoptimized: true },
+  trailingSlash: true,
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
